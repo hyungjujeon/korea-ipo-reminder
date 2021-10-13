4909d001d1a7e733b5608a6515f28655468b1f64
hyungjujeon <gudwn1549@gmail.com>
2021-10-13 21:30:31 +0900

    docs: change test.py to test heroku

--- src/test.py
+++ src/test.py
@@ -6,17 +6,25 @@ import reminder.telegram_bot as telegram_bot
 
 if __name__ == '__main__':
     today = datetime.now()
-    if today.hour == 8:
-        ipo_data_list = crawler_ipostock.get_ipo_data_list(today)
-        bid_post_id = tistory.write_new_post(ipo_data_list[:3], today, 'test')
-        ipo_post_id = tistory.write_new_post(ipo_data_list[3:], today, 'test')
+    print('heroku 시간', today.hour)
+    ipo_data_list = crawler_ipostock.get_ipo_data_list(today)
+    bid_post_id = tistory.write_new_post(ipo_data_list[:3], today, 'test')
+    ipo_post_id = tistory.write_new_post(ipo_data_list[3:], today, 'test')
 
-        telegram_bot.send_message_for_test(ipo_data_list[:3], bid_post_id, today)
-        telegram_bot.send_message_for_test(ipo_data_list[3:], ipo_post_id, today)
-    else:
-        ipo_data_list = crawler_ipostock.get_ipo_data_list(today)
-        bid_post_id = tistory.write_new_post(ipo_data_list[:3], today, 'main')
-        ipo_post_id = tistory.write_new_post(ipo_data_list[3:], today, 'main')
+    telegram_bot.send_message_for_test(ipo_data_list[:3], bid_post_id, today)
+    telegram_bot.send_message_for_test(ipo_data_list[3:], ipo_post_id, today)
 
-        telegram_bot.send_message(ipo_data_list[:3], bid_post_id, today)
-        telegram_bot.send_message(ipo_data_list[3:], ipo_post_id, today)
+    # if today.hour == 8:
+    #     ipo_data_list = crawler_ipostock.get_ipo_data_list(today)
+    #     bid_post_id = tistory.write_new_post(ipo_data_list[:3], today, 'test')
+    #     ipo_post_id = tistory.write_new_post(ipo_data_list[3:], today, 'test')
+    #
+    #     telegram_bot.send_message_for_test(ipo_data_list[:3], bid_post_id, today)
+    #     telegram_bot.send_message_for_test(ipo_data_list[3:], ipo_post_id, today)
+    # else:
+    #     ipo_data_list = crawler_ipostock.get_ipo_data_list(today)
+    #     bid_post_id = tistory.write_new_post(ipo_data_list[:3], today, 'main')
+    #     ipo_post_id = tistory.write_new_post(ipo_data_list[3:], today, 'main')
+    #
+    #     telegram_bot.send_message(ipo_data_list[:3], bid_post_id, today)
+    #     telegram_bot.send_message(ipo_data_list[3:], ipo_post_id, today)
