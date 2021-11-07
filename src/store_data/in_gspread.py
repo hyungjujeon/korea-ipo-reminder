4cfa5a081f4dc254b7300ab23cb3f45771aa952b
hyungjujeon <gudwn1549@gmail.com>
2021-11-08 02:38:06 +0900

    refactor: 공모주 데이터 클래스화에 따른 형변환
    - convert_data_format 메소드 내 데이터 할당 방식 변환

--- src/store_data/in_gspread.py
+++ src/store_data/in_gspread.py
@@ -1,9 +1,8 @@
 from datetime import datetime
 from time import sleep
 import gspread
-import src.column_description as cd
 from oauth2client.service_account import ServiceAccountCredentials
-import src.crawler.from_ipostock as crawler_ipostock
+import src.crawler.ipo_crawler as Crawler
 
 def get_google_spreadsheet(sheet_name):
     scope = [
@@ -21,39 +20,57 @@ def get_google_spreadsheet(sheet_name):
 
     return worksheet
 
-def convert_data_format(row_data):
-    weekdays = {0: ' (월)', 1: ' (화)', 2: ' (수)', 3: ' (목)', 4: ' (금)', 5: ' (토)', 6: ' (일)'}
-    row_data[cd.IpoData.BIDDING_START] += weekdays[datetime.strptime(row_data[cd.IpoData.BIDDING_START], "%Y.%m.%d").weekday()]
-    row_data[cd.IpoData.BIDDING_FINISH] += weekdays[datetime.strptime(row_data[cd.IpoData.BIDDING_FINISH], "%Y.%m.%d").weekday()]
-    row_data[cd.IpoData.REFUND_DATE] += weekdays[datetime.strptime(row_data[cd.IpoData.REFUND_DATE], "%Y.%m.%d").weekday()]
-    row_data[cd.IpoData.IPO_DATE] += weekdays[datetime.strptime(row_data[cd.IpoData.IPO_DATE], "%Y.%m.%d").weekday()]
+def convert_data_format(ipo_data):
+    row_data = [ipo_data.company_name]
 
-    row_data[cd.IpoData.UNDERWRITER] = ', '.join(row_data[cd.IpoData.UNDERWRITER])
-    row_data[cd.IpoData.ALLOCATED_SHARE_NUM] = ', '.join(map(str, row_data[cd.IpoData.ALLOCATED_SHARE_NUM]))
-    if row_data[cd.IpoData.COMPETITION_RATIO] == 0:
-        row_data[cd.IpoData.COMPETITION_RATIO] = '미표기'
-    if row_data[cd.IpoData.COMMITMENT_RATIO] == 0:
-        row_data[cd.IpoData.COMMITMENT_RATIO] = '미표기'
+    weekdays = {0: ' (월)', 1: ' (화)', 2: ' (수)', 3: ' (목)', 4: ' (금)', 5: ' (토)', 6: ' (일)'}
+
+    bidding_start = weekdays[datetime.strptime(ipo_data.bidding_start, "%Y.%m.%d").weekday()]
+    bidding_finish = weekdays[datetime.strptime(ipo_data.bidding_finish, "%Y.%m.%d").weekday()]
+    refund = weekdays[datetime.strptime(ipo_data.refund, "%Y.%m.%d").weekday()]
+    go_public = weekdays[datetime.strptime(ipo_data.go_public, "%Y.%m.%d").weekday()]
+    competition_ratio = (ipo_data.competition_ratio if ipo_data.competition_ratio != 0 else '미표기')
+    commitment_ratio = (ipo_data.commitment_ratio if ipo_data.commitment_ratio != 0 else '미표기')
+
+    row_data.append(bidding_start)
+    row_data.append(bidding_finish)
+    row_data.append(refund)
+    row_data.append(go_public)
+    row_data.append(ipo_data.band_price_low)
+    row_data.append(ipo_data.band_price_high)
+    row_data.append(ipo_data.offering_price)
+    row_data.append(ipo_data.offering_amount)
+    row_data.append(ipo_data.total_num_of_new_stocks)
+    row_data.append(ipo_data.ratio_of_new_stocks)
+    row_data.append(ipo_data.total_num_of_stock_after_ipo)
+    row_data.append(ipo_data.num_of_stock_sale_available)
+    row_data.append(ipo_data.ratio_of_sale_available)
+    row_data.append(ipo_data.num_of_stock_lockup)
+    row_data.append(ipo_data.ratio_of_lockup)
+    row_data.append(ipo_data.underwriter)
+    row_data.append(ipo_data.num_of_stock_allocated)
+    row_data.append(competition_ratio)
+    row_data.append(commitment_ratio)
 
     return row_data
 
 def write_on_spreadsheet(spreadsheet, ipo_data_list):
     for ipo_data in ipo_data_list:
-        ipo_data = convert_data_format(ipo_data)
-        spreadsheet.append_row(ipo_data)
+        row_data = convert_data_format(ipo_data)
+        spreadsheet.append_row(row_data)
 
 def write_on_spreadsheet_per_month(year, start, end):
-    for i in range(start, end+1):
-        url_list = crawler_ipostock.get_month_ipo_urls_list(year, i)
+    for month in range(start, end+1):
+        crawler = Crawler.CrawlerIpoStock()
+        ipo_url_list = Crawler.get_monthly_ipo_url_list(year, month)
+
         ipo_data_list = []
-        for url in url_list:
-            ipo_data = crawler_ipostock.crawl_ipo_info(url)
-            if ipo_data:
+        for url in ipo_url_list:
+            if url:
+                ipo_data = crawler.crawl_ipo_url(url)
                 ipo_data_list.append(ipo_data)
 
         ipo_spreadsheet = get_google_spreadsheet('전체정보')
         write_on_spreadsheet(ipo_spreadsheet, ipo_data_list)
-        print('API Write Request 제한을 위해, 40초 휴식합니다.')
+        print('Considering the api write request limit, sleep for 40 seconds')
         sleep(40)
-
-#write_on_spreadsheet_per_month('2021', 9, 9)
