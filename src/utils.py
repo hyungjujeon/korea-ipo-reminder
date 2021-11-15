bcf0948171bbb6d87b743a6eea7b9d2da9635c2a
hyungjujeon <gudwn1549@gmail.com>
2021-11-15 13:55:08 +0900

    add: 하나금융투자 수수료 2000원으로 수정

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ weekdays = {0: '(월)', 1: '(화)', 2: '(수)', 3: '(목)', 4: '(금)', 5: '(토
 
 
 def get_bidding_fee(underwriter_name):
-    if underwriter_name in ['미래에셋증권', '한국투자증권', '삼성증권', 'SK증권', '대신증권', '신영증권', '현대차증권']:
+    if underwriter_name in ['미래에셋증권', '한국투자증권', '하나금융투자', '삼성증권', 'SK증권', '대신증권', '신영증권', '현대차증권']:
         return 2000
     elif underwriter_name == 'KB증권':
         return 1500
