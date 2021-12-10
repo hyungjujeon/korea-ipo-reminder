7571ae98f685548abb5d010c7240c0fb68c34fb4
hyungjujeon <gudwn1549@gmail.com>
2021-12-10 15:00:58 +0900

    feat: 청약 수수료 변경
    - 교보증권 0원에서 1000원으로
    - 하이투자증권 1000원에서 2000원으로

--- src/utils.py
+++ src/utils.py
@@ -5,11 +5,11 @@ weekdays = {0: '(월)', 1: '(화)', 2: '(수)', 3: '(목)', 4: '(금)', 5: '(토
 
 
 def get_bidding_fee(underwriter_name):
-    if underwriter_name in ['미래에셋증권', '한국투자증권', '하나금융투자', '삼성증권', 'SK증권', '대신증권', '신영증권', '현대차증권']:
+    if underwriter_name in ['미래에셋증권', '한국투자증권', '하나금융투자', '삼성증권', 'SK증권', '대신증권', '신영증권', '현대차증권', '하이투자증권']:
         return 2000
     elif underwriter_name == 'KB증권':
         return 1500
-    elif underwriter_name in ['한화투자증권', '하이투자증권']:
+    elif underwriter_name in ['한화투자증권', '교보증권']:
         return 1000
     else:
         return 0
