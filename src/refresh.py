422068af6b067fc56d5ba60086ca8d7116e47417
hyungjujeon <gudwn1549@gmail.com>
2022-01-01 22:37:22 +0900

    feat: 오타 수정 및 스프레드시트에서 DB로 데이터 옮기기
    - gs_to_postgresql 메서드를 통해 이미 스프레드 시트에 있는
      데이터를 DB로 옮길 수 있도록 메서드 생성
    - 청약 시작 2틀 전 데이터를 넣어야되기 때문에 36L 오타를
      tomorrow_bidding_data_list로 변경

--- src/refresh.py
+++ src/refresh.py
@@ -3,6 +3,23 @@ from crawler.ipo_crawler import get_bidding_data_list, get_ipo_data_list, get_ip
 from reminder.tistory import TistoryPost
 from reminder.telegram_bot import TelegramMessage
 from src.store_data.in_gspread import GoogleSpreadSheet
+from src.store_data.in_database import Query
+
+
+def gs_to_postgresql(row_range):
+    query = Query()
+    for i in row_range:
+        row_data = gs.get_all_values_from_row(i)
+        for j in range(1, 5):
+            row_data[j] = row_data[j][:10]
+        for k in range(20, 27, 2):
+            row_data[k] = row_data[k].replace('%', '')
+
+        before_offering_data = "'" + "', '".join(row_data[:19]) + "'"
+        after_ipo_data = "'" + "', '".join(row_data[19:]) + "'"
+
+        query.insert_data('ipo_reminder', 'before_offering', before_offering_data)
+        query.insert_data('ipo_reminder', 'after_ipo', after_ipo_data)
 
 if __name__ == '__main__':
     today = datetime.utcnow() + timedelta(hours=9) - timedelta(days=1)
@@ -16,7 +33,7 @@ if __name__ == '__main__':
     today_ipo_data_list = get_ipo_data_list(today)
 
     try:
-        gs.append_row_before_ipo(tomorrow_ipo_data_list[BiddingStatus.START_TOMORROW])
+        gs.append_row_before_ipo(tomorrow_bidding_data_list[BiddingStatus.START_TOMORROW])
     except Exception as e:
         print(f'내일 모레 청약시작 종목 없음 : {e}')
 
