b3fa5bb55fa2233a5248955cc4f07f37468dd410
hyungjujeon <gudwn1549@gmail.com>
2022-01-16 19:55:57 +0900

    feat: 청약 및 배정현황 정보 가져오기

--- src/crawler/dart.py
+++ src/crawler/dart.py
@@ -32,6 +32,7 @@ def get_report_url(company_name):
             driver.quit()
             return report_url
 
+
 def get_target_subtitle_url(report_url):
     driver = webdriver.Chrome('../../chromedriver')
     target_subtitle_url = []
@@ -49,6 +50,7 @@ def get_target_subtitle_url(report_url):
                 driver.quit()
                 return target_subtitle_url
 
+
 def crawl_overview(url):
     driver = webdriver.Chrome('../../chromedriver')
     driver.get(url)
@@ -62,6 +64,7 @@ def crawl_overview(url):
 
     driver.quit()
     return data
+
 
 def crawl_underwriter_table(table):
     rows = table.find('tbody').find_all('tr')
@@ -82,6 +85,36 @@ def crawl_underwriter_table(table):
     return [', '.join(underwriter_list), ', '.join(allocated_stock_num_list), ', '.join(allocated_stock_ratio_list)]
 
 
+def crawl_bidding_result_table(table):
+    rows = table.find('tbody').find_all('tr')
+    # 구분
+    institution_list = []
+    # 최초 배정
+    initial_allocated_stock_num_list = []
+    # 최종 배정
+    final_allocated_stock_num_list = []
+    public_bidding_result = None
+
+    for i in range(len(rows) - 1):
+        tds = rows[i].find_all('td')
+        institution = tds[0].text.replace('(주)', '').strip()
+        initial_allocated_stock_num = int(tds[1].text.replace(',', ''))
+        final_allocated_stock_num = int(tds[8].text.replace(',', ''))
+
+        if '일반' in institution:
+            public_bidding_num = int(tds[3].text.replace(',', '')) # 일반 투자자 청약 건수
+            public_bidding_quantity = int(tds[4].text.replace(',', '')) # 일반 투자자 청약 수량
+            public_ratio = public_bidding_quantity / final_allocated_stock_num # 비례 경쟁률
+            public_bidding_result = [public_bidding_num, public_bidding_quantity, public_ratio]
+
+        institution_list.append(institution)
+        initial_allocated_stock_num_list.append(initial_allocated_stock_num)
+        final_allocated_stock_num_list.append(final_allocated_stock_num)
+
+    return [', '.join(institution_list), ', '.join(initial_allocated_stock_num_list),
+            ', '.join(final_allocated_stock_num_list)] + public_bidding_result
+
+
 def crawl_bidding_result(url):
     driver = webdriver.Chrome('../../chromedriver')
     driver.get(url)
@@ -91,6 +124,7 @@ def crawl_bidding_result(url):
     tables = soup.find_all('table', {'border': '1'})
 
     underwriter_table_data = crawl_underwriter_table(tables[1])
+    bidding_result_table_data = crawl_bidding_result_table(tables[2])
 
     for table in tables:
         try:
